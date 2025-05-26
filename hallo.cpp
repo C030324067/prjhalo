22ed7ca9ef880d5db570498b44a45de81d826eac
Febia Nurwulan <febianurwln@gmail.com>
2025-05-26 14:55:26 +0800

    memasukan data 5 buah nama  ke dalam array string

--- hallo.cpp
+++ hallo.cpp
@@ -2,5 +2,12 @@
 using namespace std;
 int main() {
     char nama[5][20];
+
+    strcpy(nama[0], "febia"); 
+    strcpy(nama[0], "risma"); 
+    strcpy(nama[0], "nadya"); 
+    strcpy(nama[0], "mutiara"); 
+    strcpy(nama[0], "dita"); 
+
     return 0;
 }
